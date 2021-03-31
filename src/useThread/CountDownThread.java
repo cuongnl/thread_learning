5449252c7de820bd826d0615fd09340cbb84d007
cuongnlds <cuongnlds@gmail.com>
2021-03-31 23:17:42 +0700

    add source

---
+++ src/useThread/CountDownThread.java
@@ -0,0 +1,19 @@
+package useThread;
+
+public class CountDownThread extends Thread {
+
+	@Override
+	public void run() {
+
+		int count = 10;
+		for (int i = count; i > 0; i--) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("het gio");
+	}
+}
---
+++ src/useThread/Main.java
@@ -0,0 +1,8 @@
+package useThread;
+
+public class Main {
+	public static void main(String[] args) {
+		CountDownThread countDownThread = new CountDownThread();
+		countDownThread.start();
+	}
+}
